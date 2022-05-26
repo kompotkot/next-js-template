d974ae86fa7ed596b49eb3950a900d71167a9f19
kompotkot <zenitsws@gmail.com>
2022-05-26 19:11:14 +0000

    Inital version of template

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+
+const Index: NextPage = () => {
+	return (
+		<div>
+			<Head>
+				<title>Template</title>
+				<meta name="description" content="Template of next app" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<main></main>
+
+			<footer>Powered by kompotkot</footer>
+		</div>
+	)
+}
+
+export default Index
