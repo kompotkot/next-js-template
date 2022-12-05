b9fc3c6263dda4caa20a352f58f1a4d5663472f3
kompotkot <sumarokovsergei@yandex.ru>
2022-12-05 17:31:19 +0000

    Optimized header and fixed body bug

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,32 +6,35 @@ import styles from "../styles/Layout.module.css"
 
 const Layout = ({ children, title = "Template", accountName }) => {
     const [lightTheme, setLightTheme] = useState<number>(0)
-    const [textMainColor, setTextMainColor] = useState(["white", "#141A26"])
-    const [backgroundMainColor, setBackgroundMainColor] = useState([
-        "#141A26",
-        "white",
-    ])
+
+    const [language, setLanguage] = useState<string>("EN")
 
     const [accountNavShown, setAccountNavShown] = useState<boolean>(false)
+    const [settingsNavShown, setSettingsNavShown] = useState<boolean>(false)
 
     const switchTheme = () => {
         lightTheme === 0 ? setLightTheme(1) : setLightTheme(0)
     }
+    const switchLanguage = () => {
+        language === "RU" ? setLanguage("EN") : setLanguage("RU")
+    }
 
-    useEffect(() => {
-        setTextMainColor(["white", "#141A26"])
-        setBackgroundMainColor(["#141A26", "white"])
-    }, [])
+    useEffect(() => {}, [])
+
+    const textMainColor = ["white", "#141A26"]
+    const backgroundMainColor = ["#141A26", "white"]
+    const backgroundShadowColor = ["#161d2b", "#f5f5f5"]
 
     const accountNav = (
         <nav
             className={styles.inner_nav}
             onMouseEnter={() => setAccountNavShown(true)}
             onMouseLeave={() => setAccountNavShown(false)}
+            style={{ backgroundColor: backgroundShadowColor[lightTheme] }}
         >
             <ul className={styles.site_nav}>
                 <li className={styles.nav_menu}>
-                    <Link href="/settings">
+                    <Link href="/account">
                         <p>Account settings</p>
                     </Link>
                 </li>
@@ -40,6 +43,24 @@ const Layout = ({ children, title = "Template", accountName }) => {
                         <p>Logout</p>
                     </Link>
                 </li>
+            </ul>
+        </nav>
+    )
+
+    const settingsNav = (
+        <nav
+            className={styles.inner_nav}
+            onMouseEnter={() => setSettingsNavShown(true)}
+            onMouseLeave={() => setSettingsNavShown(false)}
+            style={{ backgroundColor: backgroundShadowColor[lightTheme] }}
+        >
+            <ul className={styles.site_nav}>
+                {/* <li
+                    className={styles.nav_menu}
+                    onClick={() => switchLanguage()}
+                >
+                    <p>Switch language: {language}</p>
+                </li> */}
                 <li className={styles.nav_menu} onClick={() => switchTheme()}>
                     <p>Switch theme</p>
                 </li>
@@ -53,65 +74,94 @@ const Layout = ({ children, title = "Template", accountName }) => {
                 <title>{title}</title>
                 <meta name="description" content="Template of next app" />
                 <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Nunito"
+                />
             </Head>
-            <body
+            <div
+                className={styles.container}
                 style={{
                     color: textMainColor[lightTheme],
                     backgroundColor: backgroundMainColor[lightTheme],
                 }}
             >
-                <div className={styles.container}>
-                    <header>
-                        <nav>
-                            <ul className={styles.site_nav}>
-                                <li className={styles.nav_logo}>
-                                    <Link href="/">
-                                        <h1>Logo name</h1>
-                                    </Link>
-                                </li>
-                                <li className={styles.nav_menu}>
-                                    <Link href="/messages">
-                                        <p>Messages</p>
-                                    </Link>
-                                </li>
-                                <li className={styles.nav_menu}>
-                                    <Link href="/photos">
-                                        <p>Photos</p>
-                                    </Link>
-                                </li>
-                                <li className={styles.nav_menu}>
-                                    <Link href="/music">
-                                        <p>Music</p>
-                                    </Link>
-                                </li>
-                                <li className={styles.nav_menu}>
-                                    <Link href="/videos">
-                                        <p>Videos</p>
-                                    </Link>
-                                </li>
-                                <li className={styles.nav_menu}>
-                                    <Link href="/places">
-                                        <p>Places</p>
-                                    </Link>
-                                </li>
-                                <li
-                                    className={styles.nav_right}
-                                    onClick={() =>
-                                        setAccountNavShown(!accountNavShown)
+                <header>
+                    <nav>
+                        <ul
+                            className={styles.site_nav}
+                            style={{
+                                backgroundColor:
+                                    backgroundShadowColor[lightTheme],
+                            }}
+                        >
+                            <li className={styles.nav_logo}>
+                                <Link href="/">
+                                    <h1>logo name</h1>
+                                </Link>
+                            </li>
+                            <li className={styles.nav_menu}>
+                                <Link href="/messages">
+                                    <p>Messages</p>
+                                </Link>
+                            </li>
+                            <li className={styles.nav_menu}>
+                                <Link href="/photos">
+                                    <p>Photos</p>
+                                </Link>
+                            </li>
+                            <li className={styles.nav_menu}>
+                                <Link href="/music">
+                                    <p>Music</p>
+                                </Link>
+                            </li>
+                            <li className={styles.nav_menu}>
+                                <Link href="/videos">
+                                    <p>Videos</p>
+                                </Link>
+                            </li>
+                            <li className={styles.nav_menu}>
+                                <Link href="/places">
+                                    <p>Places</p>
+                                </Link>
+                            </li>
+                            <li
+                                className={styles.nav_account}
+                                onMouseEnter={() => setAccountNavShown(true)}
+                                onMouseLeave={() => setAccountNavShown(false)}
+                            >
+                                <p>{accountName}</p>
+                            </li>
+                            <li
+                                className={styles.nav_settings}
+                                onMouseEnter={() => setSettingsNavShown(true)}
+                                onMouseLeave={() => setSettingsNavShown(false)}
+                            >
+                                <img
+                                    className={styles.icon}
+                                    src={
+                                        lightTheme
+                                            ? "setting-gear-dark.svg"
+                                            : "setting-gear-light.svg"
                                     }
-                                >
-                                    <p>{accountName}</p>
-                                </li>
-                            </ul>
-                        </nav>
-                        {accountNavShown && accountNav}
-                    </header>
-                    <main className={styles.main}>{children}</main>
-                    <footer className={styles.footer}>
-                        <p>© 2022 kompotkot</p>
-                    </footer>
-                </div>
-            </body>
+                                    alt="Settings"
+                                />
+                            </li>
+                        </ul>
+                    </nav>
+                    {accountNavShown && accountNav}
+                    {settingsNavShown && settingsNav}
+                </header>
+                <main className={styles.main}>{children}</main>
+                <footer
+                    className={styles.footer}
+                    style={{
+                        backgroundColor: backgroundShadowColor[lightTheme],
+                    }}
+                >
+                    <p>© 2022 kompotkot</p>
+                </footer>
+            </div>
         </>
     )
 }
